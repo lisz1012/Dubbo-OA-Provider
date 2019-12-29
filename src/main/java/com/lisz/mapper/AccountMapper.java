bc06e4a8267118826e1441ea428299248796bf01
lisz1012 <lisz1012@163.com>
2019-12-29 00:46:38 -0800

    Add notes

--- src/main/java/com/lisz/mapper/AccountMapper.java
+++ src/main/java/com/lisz/mapper/AccountMapper.java
@@ -3,6 +3,7 @@ package com.lisz.mapper;
 import com.lisz.entity.Account;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -15,5 +16,6 @@ public interface AccountMapper extends MyBatisBaseDao<Account, Integer, AccountE
 
 	void addRolesForAccount(int[] roleIds, int accountId);
 
-	Account findByUsernameAndPassword(String username, String password);
+	// 虽然变量名与mapper.xml文件中SQL中#{}内的一样的话，有时也能正确得出去而已结果，但有时不行，不知道为什么，最好写上@Param注解
+	Account findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 }
